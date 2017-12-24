eda473c09d0c718e1a0af220b38b11ff60dd51db
andela-akoya <adegboyega.koya@andela.com>
2017-12-24 09:52:09 +0100

    implement the delete functionality for the portfolio resource

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,4 +39,12 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully deleted'}
+    end
+  end
 end
