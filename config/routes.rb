67f4efed7f31ae23418e9aac48878939708b0c86
andela-akoya <adegboyega.koya@andela.com>
2017-12-25 04:08:22 +0100

    implement custom action for toggling blog status

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+  resources :blogs do
+    get 'toggle_status', on: :member
+  end
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
