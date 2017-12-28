15fb6a9f2911f2721b78bad5372222c7153647ca
andela-akoya <adegboyega.koya@andela.com>
2017-12-28 15:33:18 +0100

    add session data for dynamic source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
+
+  before_action :set_source
+
+  def set_source
+    if data = params[:q]
+      session[:source] = data
+    end
+  end
 end
